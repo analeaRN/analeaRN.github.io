6905ae4b713d37ab73da4e8863a2befe6e9cb391
nishiana <29620189+analeaRN@users.noreply.github>
2021-07-11 12:01:01 -0700

    finished

--- week1/separated_py_files/big_fibonacci.py
+++ week1/separated_py_files/big_fibonacci.py
@@ -1,2 +1,14 @@
-def big_fibonacci():
-    pass
+def big_fibonacci(n):
+    if n < 0:
+        return 0
+    
+    current = 1
+    a = 1
+    b = 1
+    
+    while len(str(current)) < n:
+        current = sum(a, b)
+        a = b
+        b = current
+    
+    return current
