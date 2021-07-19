d536df35e27b8563fdbe8b61377034da77cd13e2
nishiana <29620189+analeaRN@users.noreply.github>
2021-07-18 19:00:21 -0700

    added pictures per assignment details

--- week2/app.py
+++ week2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -10,8 +10,8 @@ def start():
     text = """You decide to make a cake, but you gotta decide what flavor it's gonna be first!"""
 
     choices = [
-        ('chocolate',"Make a chocolate cake! 🍫"),
-        ('carrot_cake',"Put together the best carrot cake the world will ever see. 🥕")
+        ('chocolate',"Make a chocolate cake! 🍫", url_for('static', filename='choco_cake.JPG')),
+        ('carrot_cake',"Put together the best carrot cake the world will ever see. 🥕", url_for('static', filename='carrot_cake.JPG'))
     ]
 
     return render_template('adventure.html', title=title, text=text, choices=choices)
@@ -25,8 +25,8 @@ def chocolate():
     text = """Wow good choice. Please make me some too. But... what about the filling?"""
 
     choices = [
-        ('there_is_nothing_else',"MORE CHOCOLATE! 🍫"),
-        ('there_is_nothing_else',"Use Cream cheese as a filling.")
+        ('there_is_nothing_else',"MORE CHOCOLATE! 🍫", url_for('static', filename='choco_fill.JPG')),
+        ('there_is_nothing_else',"Use Cream cheese as a filling.", url_for('static', filename='cream_cheese_fill.JPG'))
     ]
 
     return render_template('adventure.html', title=title, text=text, choices=choices)
@@ -37,9 +37,11 @@ def carrot_cake():
     
     text = """Sorry you don't have anny carrots. Maybe you can make a ^ cake?"""
 
+    end_photo = url_for('static', filename='no_more_carrots.JPG')
+    
     choices = []
 
-    return render_template('adventure.html', title=title, text=text, choices=choices)
+    return render_template('adventure.html', title=title, text=text, choices=choices, end_photo=end_photo)
 
 
 
@@ -49,6 +51,8 @@ def there_is_nothing_else():
     
     text = """You make your cake, and you ate it too. ✨ What a wonderful day ✨"""
 
+    end_photo = url_for('static', filename='end.JPG')
+
     choices = []
 
-    return render_template('adventure.html', title=title, text=text, choices=choices)
+    return render_template('adventure.html', title=title, text=text, choices=choices, end_photo=end_photo)
